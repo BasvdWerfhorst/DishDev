189c196e16dc6870d2f465e99b2666c8d3fa5532
Bas <6059520@mborijnland.nl>
2026-03-12 13:31:40 +0100

    .

---
+++ inc/footer.php
@@ -0,0 +1,22 @@
+<article id="footer-column left">
+    <h3>Email:</h3>
+    <p>info@veganfoodamsterdam.nl</p>
+    <p>support@veganfoodamsterdam.nl</p>
+    <p>jobs@veganfoodamsterdam.nl</p>
+</article>
+
+<article id="footer-column center">
+    <p>Telefoon: 088-2321451</p>
+</article>
+
+<article id="footer-column right">
+    <h3>Social media</h3>
+    <article id="social-icons">
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+    </article>
+    <p class="made-by">© Gemaakt door </p>
+</article>
+
+
---
+++ inc/header.php
@@ -0,0 +1,14 @@
+<section class="navBar">
+    <picture class="logo">
+        <img src="img/logo_vegan_food_plaatje.png" alt="Vegan food logo">
+    </picture>
+    <nav class="navLinks">
+        <a href="index.php">Home</a>
+        <a href="menu.php">Menu</a>
+        <a href="contact.php">Contact</a>
+        <a href="reserveren.php">Reserveren</a>
+        <a href="vacatures.php">Vacaturens</a>
+    </nav>
+    
+</section>
+
