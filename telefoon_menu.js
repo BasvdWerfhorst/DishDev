84ec1d57d0b9e69d74c4a7e04d246b886eaee03c
Bas <6059520@mborijnland.nl>
2026-03-12 12:48:23 +0100

    .

--- telefoon_menu.js
+++
@@ -1,9 +0,0 @@
-const knop = document.querySelector('.hamburgerMenu');
-const nav = document.querySelector('.navLinks');
-const main = document.querySelector('main');
-
-knop.addEventListener('click', function() {
-    knop.classList.toggle('active');
-    nav.classList.toggle('active');
-    main.classList.toggle('hidden');
-});
