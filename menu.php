134b1bcb4dc4f880cd3e28d9f90f9b3e949bc2a6
Bas <6059520@mborijnland.nl>
2026-03-12 14:17:01 +0100

    menu heeft menu

--- menu.php
+++ menu.php
@@ -14,23 +14,45 @@
 </header>
 
 <main>
-<section id="menuInfo">
-    <section id="Dinner" class="menuSection">
-        <article class="menuCategory">
-            <h2 class="menuHeading">voorgerecht</h2>
-            <article class="menuItems">
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <p>5</p>
-                <p>6</p>
-                <p>7</p>
-                <p>8</p>
-            </article>
-        </article>
-    </section>
-</section>
+<h3>voorgerecht</h3>
+<p>Romige Pompoensoep</p>
+<p>Gefrituurde ArIsjok</p>
+<p>BruscheDa Tricolore</p>
+
+<h3>hoofdgerechten</h3>
+<p>Portobello Wellington</p>
+<p>Jackfruit Rendang</p>
+<p>Gnocchi al Tartufo</p>
+<p>Beyond Burger Deluxe</p>
+
+<h3>dessert</h3>
+<p>Chocolade Lava Cake</p>
+<p>Cheesecake van de Dag</p>
+<p>Crème Brûlée</p>
+
+<h3>bijgerechten</h3>
+<p>Zoete Aardappelfriet</p>
+<p>Gemengde Salade</p>
+<p>Geroosterde Seizoensgroenten</p>
+
+<h3>drankjes</h3>
+<h3>warme dranken</h3>
+<p>Espresso</p>
+<p>Cappuccino</p>
+<p>Thee</p>
+<p>Verse muntthee</p>
+<h3>koude dranken</h3>
+<p>Huisgemaakte ijsthee</p>
+<p>Vers geperst sinaasappelsap</p>
+<p>Kombucha van de dag</p>
+<p>Biologische frisdranken</p>
+<h3>wijnen</h3>
+<h3>witte wijn</h3>
+<p>Biologische Chardonnay</p>
+<p>Biologische Sauvignon Blanc</p>
+<h3>roode wijn</h3>
+<p>Biologische Merlot</p>
+<p>Biologische Tempranillo</p>
 
 </main>
 
