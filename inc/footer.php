99e51f961251259f21defce106470e8464bbf693
Wessel Harm <harmwessel@gmail.com>
2026-03-16 10:44:39 +0100

    Footer gemaakt, deels gedecoreerd
    Footer gemaakt, naar benedenverplaatst (aanpasbaar), text correct geplaatst met grid, met groen aangegeven waar was moet.

--- inc/footer.php
+++ inc/footer.php
@@ -1,19 +1,59 @@
-<article id="footer-column left">
-    <p>broodje kip</p>
-</article>
+<footer class="footer-BGC">
+    <section class="footer-container">
+        <!-- . page title . -->
+        <article class="footerbox-1">Vegen Food <br> Amsterdam</article>
+        <!-- . Useful links text . -->
+        <article class="footerbox-2">Useful Links</article>
+        <!-- . Mady by text . -->
+        <article class="footerbox-3">Made by:</article>
+        <!-- . Web Info Copyright ect . -->
+        <article class="footerbox-4">This website does not claim ownership of any copyrighted materials displayed unless otherwise stated. All content, including images, text, videos, and other media, belongs to their respective owners.</article>
+        <!-- . Links(provecy, contact ect.) . -->
+        <article class="footerbox-5">Privacy <br> Contact <br> Vacatures</article>
+        <!-- . Wessel & bas . -->
+        <article class="footerbox-6">Wessel & Bas</article>
+        <!-- . Copyright mark . -->
+        <article class="footerbox-7">© all rights reserved by Vegan Food</article>
+        <!-- .Social media logo's?. -->
+        <article class="footerbox-8"></article>
+    </section>
+</footer>
 
-<article id="footer-column center">
-    <p>klaar zo</p>
-</article>
 
-<article id="footer-column right">
-    <h3>drekset</h3>
-    <article id="social-icons">
-        <a href="#"></a>
-        <a href="#"></a>
-        <a href="#"></a>
-    </article>
-    <p class="made-by"></p>
-</article>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <!-- require_once 'inc/footer.php' -->
