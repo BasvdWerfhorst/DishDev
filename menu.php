c4d26561b8c9d909337352e7c774e25cef459da2
Bas <6059520@mborijnland.nl>
2026-03-19 10:13:07 +0100

    menu met deelknoppen
    de html voor de menu is af en ik heb de menu verdeelt in 4 knoppen om delen van de menu te zien

--- menu.php
+++ menu.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="style/style.css">
     <script src="script/menu.js" defer></script>
 </head>
+
 <body>
     
 <header>
@@ -14,45 +15,58 @@
 </header>
 
 <main>
-<h3>voorgerecht</h3>
-<p>Romige Pompoensoep</p>
-<p>Gefrituurde ArIsjok</p>
-<p>BruscheDa Tricolore</p>
+<section class="menuSection">
+    <button class="menuButton" onclick="toonMenu('eten')">Eten</button>
+    <button class="menuButton" onclick="toonMenu('dessert')">Dessert</button>
+    <button class="menuButton" onclick="toonMenu('drinken')">Drinken</button>
+    <button class="menuButton" onclick="toonMenu('wijnen')">Wijnen</button>
+</section>
 
-<h3>hoofdgerechten</h3>
-<p>Portobello Wellington</p>
-<p>Jackfruit Rendang</p>
-<p>Gnocchi al Tartufo</p>
-<p>Beyond Burger Deluxe</p>
+<section id="eten">
+    <h3>voorgerecht</h3>
+    <p>Romige Pompoensoep</p>
+    <p>Gefrituurde ArIsjok</p>
+    <p>BruscheDa Tricolore</p>
 
-<h3>dessert</h3>
-<p>Chocolade Lava Cake</p>
-<p>Cheesecake van de Dag</p>
-<p>Crème Brûlée</p>
+    <h3>hoofdgerechten</h3>
+    <p>Portobello Wellington</p>
+    <p>Jackfruit Rendang</p>
+    <p>Gnocchi al Tartufo</p>
+    <p>Beyond Burger Deluxe</p>
 
-<h3>bijgerechten</h3>
-<p>Zoete Aardappelfriet</p>
-<p>Gemengde Salade</p>
-<p>Geroosterde Seizoensgroenten</p>
+    <h3>bijgerechten</h3>
+    <p>Zoete Aardappelfriet</p>
+    <p>Gemengde Salade</p>
+    <p>Geroosterde Seizoensgroenten</p>
+</section>
+<section id="dessert" style="display:none">
+    <h3>dessert</h3>
+    <p>Chocolade Lava Cake</p>
+    <p>Cheesecake van de Dag</p>
+    <p>Crème Brûlée</p>
+</section>
+<section id="drinken" style="display:none">
+    <h3>warme dranken</h3>
+    <p>Espresso</p>
+    <p>Cappuccino</p>
+    <p>Thee</p>
+    <p>Verse muntthee</p>
 
-<h3>drankjes</h3>
-<h3>warme dranken</h3>
-<p>Espresso</p>
-<p>Cappuccino</p>
-<p>Thee</p>
-<p>Verse muntthee</p>
-<h3>koude dranken</h3>
-<p>Huisgemaakte ijsthee</p>
-<p>Vers geperst sinaasappelsap</p>
-<p>Kombucha van de dag</p>
-<p>Biologische frisdranken</p>
-<h3>wijnen</h3>
-<h3>witte wijn</h3>
-<p>Biologische Chardonnay</p>
-<p>Biologische Sauvignon Blanc</p>
-<h3>roode wijn</h3>
-<p>Biologische Merlot</p>
-<p>Biologische Tempranillo</p>
+    <h3>koude dranken</h3>
+    <p>Huisgemaakte ijsthee</p>
+    <p>Vers geperst sinaasappelsap</p>
+    <p>Kombucha van de dag</p>
+    <p>Biologische frisdranken</p>
+</section>
+<section id="wijnen" style="display:none">
+    <h3>witte wijn</h3>
+    <p>Biologische Chardonnay</p>
+    <p>Biologische Sauvignon Blanc</p>
+
+    <h3>roode wijn</h3>
+    <p>Biologische Merlot</p>
+    <p>Biologische Tempranillo</p>
+</section>
 
 </main>
 
---
+++ script/menu.js
@@ -0,0 +1,7 @@
+function toonMenu(versie) {
+    document.getElementById('eten').style.display = 'none'
+    document.getElementById('dessert').style.display = 'none'
+    document.getElementById('drinken').style.display = 'none'
+    document.getElementById('wijnen').style.display = 'none'
+    document.getElementById(versie).style.display = 'block'
+}
