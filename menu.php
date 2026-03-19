b6a219774999f1f560c0a127cfb112dcfa319863
Bas <6059520@mborijnland.nl>
2026-03-19 10:39:26 +0100

    menu mooi op laptop

--- menu.php
+++ menu.php
@@ -24,48 +24,115 @@
 
 <section id="eten">
     <h3>voorgerecht</h3>
-    <p>Romige Pompoensoep</p>
-    <p>Gefrituurde ArIsjok</p>
-    <p>BruscheDa Tricolore</p>
+    <article class="menuRow">
+        <picture class="menuImg">
+            <img src="img/x.png" alt="x">
+        </picture>
+        <article class="menuItems">
+            <p>Romige Pompoensoep</p>
+            <p>Gefrituurde ArIsjok</p>
+            <p>BruscheDa Tricolore</p> 
+        </article>
+    </article>
+
+    <br>
 
     <h3>hoofdgerechten</h3>
-    <p>Portobello Wellington</p>
-    <p>Jackfruit Rendang</p>
-    <p>Gnocchi al Tartufo</p>
-    <p>Beyond Burger Deluxe</p>
+    <article class="menuRow">
+        <picture class="menuImg">
+            <img src="img/x.png" alt="x">
+        </picture>
+        <article class="menuItems">
+            <p>Portobello Wellington</p>
+            <p>Jackfruit Rendang</p>
+            <p>Gnocchi al Tartufo</p>
+            <p>Beyond Burger Deluxe</p>
+        </article>
+    </article>
+
+    <br>
 
     <h3>bijgerechten</h3>
-    <p>Zoete Aardappelfriet</p>
-    <p>Gemengde Salade</p>
-    <p>Geroosterde Seizoensgroenten</p>
+    <article class="menuRow">
+        <picture class="menuImg">
+            <img src="img/x.png" alt="x">
+        </picture>
+        <article class="menuItems">
+            <p>Zoete Aardappelfriet</p>
+            <p>Gemengde Salade</p>
+            <p>Geroosterde Seizoensgroenten</p>
+        </article>
+    </article>
+
 </section>
 <section id="dessert" style="display:none">
     <h3>dessert</h3>
-    <p>Chocolade Lava Cake</p>
-    <p>Cheesecake van de Dag</p>
-    <p>Crème Brûlée</p>
+        <article class="menuRow">
+        <picture class="menuImg">
+            <img src="img/x.png" alt="x">
+        </picture>
+        <article class="menuItems">
+            <p>Chocolade Lava Cake</p>
+            <p>Cheesecake van de Dag</p>
+            <p>Crème Brûlée</p>
+        </article>
+    </article>
 </section>
 <section id="drinken" style="display:none">
     <h3>warme dranken</h3>
-    <p>Espresso</p>
-    <p>Cappuccino</p>
-    <p>Thee</p>
-    <p>Verse muntthee</p>
+    <article class="menuRow">
+        <picture class="menuImg">
+            <img src="img/x.png" alt="x">
+        </picture>
+        <article class="menuItems">
+            <p>Espresso</p>
+            <p>Cappuccino</p>
+            <p>Thee</p>
+            <p>Verse muntthee</p>
+        </article>
+    </article>
+
+    <br>
 
     <h3>koude dranken</h3>
-    <p>Huisgemaakte ijsthee</p>
-    <p>Vers geperst sinaasappelsap</p>
-    <p>Kombucha van de dag</p>
-    <p>Biologische frisdranken</p>
+    <article class="menuRow">
+        <picture class="menuImg">
+            <img src="img/x.png" alt="x">
+        </picture>
+        <article class="menuItems">
+            <p>Huisgemaakte ijsthee</p>
+            <p>Vers geperst sinaasappelsap</p>
+            <p>Kombucha van de dag</p>
+            <p>Biologische frisdranken</p>
+        </article>
+        
+    </article>
 </section>
 <section id="wijnen" style="display:none">
     <h3>witte wijn</h3>
-    <p>Biologische Chardonnay</p>
-    <p>Biologische Sauvignon Blanc</p>
+    <article class="menuRow">
+        <picture class="menuImg">
+            <img src="img/x.png" alt="x">
+        </picture>
+        <article class="menuItems">
+            <p>Biologische Chardonnay</p>
+            <p>Biologische Sauvignon Blanc</p>
+        </article>
+        
+    </article>
+
+    <br>
 
     <h3>roode wijn</h3>
-    <p>Biologische Merlot</p>
-    <p>Biologische Tempranillo</p>
+    <article class="menuRow">
+        <picture class="menuImg">
+            <img src="img/x.png" alt="x">
+        </picture>
+        <article class="menuItems">
+            <p>Biologische Merlot</p>
+            <p>Biologische Tempranillo</p>
+        </article>
+    </article>
 </section>
 
 </main>
