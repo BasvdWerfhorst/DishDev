354e749d22855ce0303bd3801b6d1b3006a31b80
Bas <6059520@mborijnland.nl>
2026-03-20 17:11:14 +0100

    html structuur

--- reserveren.php
+++ reserveren.php
@@ -8,12 +8,37 @@
 </head>
 <body>
     
-<header class="Header-BGC">
+<header>
 <?php require_once 'inc/header.php' ?>
 </header>
 
 <main>
-
+<h1 id="resH1">reserveren</h1>
+<form action="" method="get" id="form">
+<label for="name">Uw naam:</label>
+<input type="text" name="name" required id="name">
+<label for="email">Uw e-mail:</label>
+<input type="email" name="email" required id="email">
+<label for="telefoonnummer">Uw telefoonnummer:</label>
+<input type="tel" name="telefoonnummer" required id="telNummer">
+<label for="aantalPersonen">aantal personen:</label>
+<input type="number" name="aantalPersonen" required min="1" max="10" id="personen">
+<section class="DateTijd">
+    <label for="date">Datum an</label>
+    <label for="tijd">d tijd:</label>
+</section>
+<section class="DateTijd">
+    <input type="date" name="date" required id="date">
+    <input type="time" name="tijd" required id="time">
+</section>  
+<label for="opmerkingen">extra informatie/opmerkingen:</label>
+<textarea 
+    id="opmerkingen" 
+    name="opmerkingen" 
+    placeholder="Typ hier...">
+</textarea>
+<input type="submit" value="reserveren" id="submitForm">
+</form>
 </main>
 
 <footer>
