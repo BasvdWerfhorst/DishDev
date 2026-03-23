582df941a4bea114d9d79bd617b5648a82a4dec0
Wessel Harm <harmwessel@gmail.com>
2026-03-23 13:47:00 +0100

    Added all meta tags
    Description may be changed per webpage.

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="This is a site called Vegan Food Amsterdam, dedicated to exploring vegan food in Amsterdam, sharing information, tips, and guides about plant-based dining and the local vegan scene.">
+    <meta name="keywords" content="vegan, vegatarish, vega amsterdam, vegan food amsterdam, vegan restaurant amsterdam, plant based restaurant amsterdam, vegan dining amsterdam, vegan food in amsterdam, vegan cuisine amsterdam, vegan menu amsterdam, vegan cafe amsterdam, plant based food amsterdam, vegan lunch amsterdam, vegan dinner amsterdam, healthy vegan food amsterdam, vegan takeaway amsterdam, vegan restaurant netherlands">
+    <meta name="author" content="Wessel & Bas">
+
     <title>DishDev</title>
     <link rel="stylesheet" href="style/style.css">
     <script src="indexpage.js" defer></script>
@@ -21,7 +25,10 @@
 </div>
 
 
-
+        <article id="aboutarticle">
+            <h1 id="about-us">About us:</h1> <br>
+            <p id="About-text">The restaurant began as a small pop-up in a borrowed kitchen, where plant-based meals were cooked for friends and curious passersby. A belief that vegan food should be comforting helped it grow a loyal following. Years later, it became a permanent spot—simple, seasonal, and built on the idea that good food can gently change habits. Check a few of our dishes below:</p>
+        </article>
 
 
       
