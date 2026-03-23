da26dbb67b0bfad365972cbc9dbe7fa86de77026
Wessel Harm <harmwessel@gmail.com>
2026-03-23 13:50:16 +0100

    Changed title accuratly to all

--- vacatures.php
+++ vacatures.php
@@ -7,8 +7,9 @@
     <meta name="description" content="This is a site called Vegan Food Amsterdam, dedicated to exploring vegan food in Amsterdam, sharing information, tips, and guides about plant-based dining and the local vegan scene.">
     <meta name="keywords" content="vegan, vegatarish, vega amsterdam, vegan food amsterdam, vegan restaurant amsterdam, plant based restaurant amsterdam, vegan dining amsterdam, vegan food in amsterdam, vegan cuisine amsterdam, vegan menu amsterdam, vegan cafe amsterdam, plant based food amsterdam, vegan lunch amsterdam, vegan dinner amsterdam, healthy vegan food amsterdam, vegan takeaway amsterdam, vegan restaurant netherlands">
     <meta name="author" content="Wessel & Bas">
-    
-    <title>DishDev</title>
+
+    <title>Vegan Food Amsterdam</title>
+
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
