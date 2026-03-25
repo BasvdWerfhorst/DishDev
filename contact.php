379f9f3164fbae12c99b6a791481a8beee701cb3
Wessel Harm <harmwessel@gmail.com>
2026-03-25 13:13:15 +0100

    Finished contact page

--- contact.php
+++ contact.php
@@ -20,21 +20,47 @@
 </header>
  
 <main>
+
+            
+                <h2 id="contact-textheader">Contact Pagina</h2>
+            
+
     <section class="container-contact">
             <article class="contact-article">
-            Beste Mensen
+                <h4 class="contact-headers">Openingstijden: </h4>
+                <p class="openingstijden-text">
+                    Maandag: 09:00 – 18:00 <br>
+                   Dinsdag: 09:00 – 18:00 <br>
+                   Woensdag: 09:00 – 18:00 <br>
+                   Donderdag: 09:00 – 21:00 <br>
+                   Vrijdag: 09:00 – 18:00 <br>
+                   Zaterdag: 10:00 – 17:00 <br>
+                   Zondag: Gesloten</p>
+
             </article>
 
             <article class="contact-article">
-            
+                <h4 class="contact-headers">Adress Gegevens</h4>
+                <p id="contactinfo-text">
+                   Vegan Food Amsterdam  Keizersgracht 212  1016DX 
+                   Amsterdam  email: info@veganfoodamsterdam.nl 
+                   Telefoonnummer: 088-2321451
+                </p>
             </article>
 
             <article class="contact-article">
-            
+                <h4 class="contact-headers">FAQ</h4>
+                <p id="contactinfo-text">
+                    What are your opening hours? See our opening hours above. <br>
+                    Do I need an appointment? Appointments are recommended. <br>
+                    How can I contact you? Via phone, email, or contact form. <br>
+                    Where are you located? Address is listed on the contact page. <br>
+                    Which payment methods are accepted? Cash and cards accepted.
+                </p>
             </article>
 
             <article class="contact-article">
-            
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d160629.9150206605!2d4.739154272277158!3d52.35476072731975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c63fb5949a7755%3A0x6600fd4cb7c0af8d!2sAmsterdam%2C%20Nederland!5e1!3m2!1snl!2suk!4v1774436046185!5m2!1snl!2suk" id="contact-maps"></iframe>
             </article>
     </section>
 
