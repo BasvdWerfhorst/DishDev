cd5a5f64e60716227c620fc16aa3e5057ffbe84f
Bas <6059520@mborijnland.nl>
2026-03-31 12:37:42 +0200

    footer met grid en met clamp

--- inc/footer.php
+++ inc/footer.php
@@ -1,35 +1,25 @@
-<footer class="footer-BGC">
-    <section class="footer-container">
-        <!-- . page title . -->
-         <a href="index.php" style="text-decoration: none;">
-        <article class="footerbox-1">Vegen Food <br> Amsterdam</article>
-        </a>
-        <!-- . Useful links text . -->
-        <article class="footerbox-2">Handige Links</article>
-        <!-- . Mady by text . -->
-        <article class="footerbox-3">Gemaakt door:</article>
-        <!-- . Web Info Copyright ect . -->
-        <article class="footerbox-4">Deze website claimt geen eigendomsrechten op het getoonde auteursrechtelijk beschermde materiaal, tenzij anders vermeld. Alle inhoud, inclusief afbeeldingen, tekst, video's en andere media, behoort toe aan de respectievelijke eigenaren.</article>
-        <!-- . Links(provecy, contact ect.) . -->
-        <article class="footerbox-5"> <a href="contact.php" >Privacy</a> <br> <a href="contact.php" >contact</a> <br> <a href="vacatures.php" >Vacatures</a></article>
-        <!-- . Wessel & bas . -->
-        <article class="footerbox-6">Wessel & Bas</article>
-        <!-- . Copyright mark . -->
-        <article class="footerbox-7">© alle rechten voorbehouden door Vegan Food</article>
-        <!-- Img Icons -->
-            <article class="footer-icons">
-               <a href="https://www.instagram.com/wessel001/"> 
-                <img src="img\Social\Insta_ICon_NB-removebg-preview.png" alt="Instagram Icon">
-               </a> 
-               <a href="https://www.youtube.com/watch?v=28E_l690_0g"> 
-                <img src="img\Social\Facebook_Icon_NB-removebg-preview.png" alt="Facebook Icon">
-                </a>
-                <a href="https://www.youtube.com/@Link017">
-                <img src="img\Social\YT_icon_NB-removebg-preview.png" alt="Youtube Icon">
-                </a>
-            </article>
-    </section>
-</footer>
+<section id="footer-container">
+    <a href="index.php" id="footerILink"><article class="gridBoxes" id="footerGrid1">Vegen Food   Amsterdam</article></a>
+    <article class="gridBoxes" id="footerGrid2">Handige Links</article>
+    <article class="gridBoxes" id="footerGrid3">Gemaakt door:</article>
+    <article class="gridBoxes" id="footerGrid4">Deze website claimt geen eigendomsrechten op het getoonde auteursrechtelijk beschermde materiaal, tenzij anders vermeld. Alle inhoud, inclusief afbeeldingen, tekst, video's en andere media, behoort toe aan de respectievelijke eigenaren.</article>
+    <article class="gridBoxes" id="footerGrid5">
+        <a href="contact.php" >Privacy</a> <br> <a href="contact.php" >contact</a> <br> <a href="vacatures.php" >Vacatures</a>
+    </article>
+    <article class="gridBoxes" id="footerGrid6">Wessel & Bas</article>
+    <article class="gridBoxes" id="footerGrid7">© alle rechten voorbehouden door Vegan Food</article>
+        <article class="gridBoxes" id="footer-icons">
+            <a href="https://www.instagram.com/wessel001/"> 
+            <img src="img\Social\Insta_ICon_NB-removebg-preview.png" alt="Instagram Icon">
+            </a> 
+            <a href="https://www.youtube.com/watch?v=28E_l690_0g"> 
+            <img src="img\Social\Facebook_Icon_NB-removebg-preview.png" alt="Facebook Icon">
+            </a>
+            <a href="https://www.youtube.com/@Link017">
+            <img src="img\Social\YT_icon_NB-removebg-preview.png" alt="Youtube Icon">
+            </a>
+        </article>
+</section>
 
 
 
