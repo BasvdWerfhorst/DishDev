a9549b681d4c4db80565ebb0b480052e48287747
Wessel Harm <harmwessel@gmail.com>
2026-03-31 12:47:43 +0100

    Descripties vertaald naar nl

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="This is a site called Vegan Food Amsterdam, dedicated to exploring vegan food in Amsterdam, sharing information, tips, and guides about plant-based dining and the local vegan scene.">
+    <meta name="description" content="Dit is een website genaamd Vegan Food Amsterdam, gewijd aan het ontdekken van veganistisch eten in Amsterdam. We delen informatie, tips en gidsen over plantaardig eten en de lokale veganistische scene.">
     <meta name="keywords" content="vegan, vegatarish, vega amsterdam, vegan food amsterdam, vegan restaurant amsterdam, plant based restaurant amsterdam, vegan dining amsterdam, vegan food in amsterdam, vegan cuisine amsterdam, vegan menu amsterdam, vegan cafe amsterdam, plant based food amsterdam, vegan lunch amsterdam, vegan dinner amsterdam, healthy vegan food amsterdam, vegan takeaway amsterdam, vegan restaurant netherlands">
     <meta name="author" content="Wessel & Bas">
 
